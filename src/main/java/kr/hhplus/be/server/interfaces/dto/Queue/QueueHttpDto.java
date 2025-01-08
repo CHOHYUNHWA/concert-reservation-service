893b7c6870a5455b6a680a78117bdac90e064b1d
CHOHYUNHWA <gusghk115@gmail.com>
2025-01-08 10:50:31 +0900

    chore: Queue 관련 HttpDto 리팩토링

---
+++ src/main/java/kr/hhplus/be/server/interfaces/dto/Queue/QueueHttpDto.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.interfaces.dto.Queue;
+
+import kr.hhplus.be.server.domain.entity.Queue;
+import kr.hhplus.be.server.support.type.QueueStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public class QueueHttpDto {
+
+    @Builder
+    public static class QueueStatusResponseDto {
+        private QueueStatus status;
+        private Long remainingQueueCount;
+
+        public static QueueStatusResponseDto of(QueueStatus status, Long remainingQueueCount) {
+            return QueueStatusResponseDto.builder()
+                    .status(status)
+                    .remainingQueueCount(remainingQueueCount).build();
+        }
+    }
+
+
+    @Getter
+    public static class CreateTokenRequestDto{
+        private Long userId;
+    }
+
+    @Builder
+    public static class CreatedTokenResponseDto {
+        private String token;
+        private LocalDateTime createdAt;
+        private LocalDateTime expiredAt;
+
+        public static CreatedTokenResponseDto of(Queue queue) {
+            return CreatedTokenResponseDto.builder()
+                    .token(queue.getToken())
+                    .createdAt(queue.getCreatedAt())
+                    .expiredAt(queue.getExpiredAt())
+                    .build();
+        }
+    }
+}
