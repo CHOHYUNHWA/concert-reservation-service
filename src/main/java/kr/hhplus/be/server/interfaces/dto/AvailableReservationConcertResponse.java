1a08daa5e91705a175e6db6230a155a35c1377f0
CHOHYUNHWA <gusghk115@gmail.com>
2025-01-09 15:40:47 +0900

    chore: 콘서트 관련 Dto 객체 정적팩토리 메서드 생성

--- src/main/java/kr/hhplus/be/server/interfaces/dto/AvailableReservationConcertResponse.java
+++ src/main/java/kr/hhplus/be/server/interfaces/dto/AvailableReservationConcertResponse.java
@@ -1,15 +1,29 @@
 package kr.hhplus.be.server.interfaces.dto;
 
+import kr.hhplus.be.server.domain.entity.Concert;
 import kr.hhplus.be.server.domain.entity.ConcertSchedule;
 import kr.hhplus.be.server.support.type.ConcertStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AvailableReservationConcertResponse {
     private Long id;
     private String title;
     private String description;
     private ConcertStatus status;
+
+    public static AvailableReservationConcertResponse of(Concert concert) {
+        return AvailableReservationConcertResponse.builder()
+                .id(concert.getId())
+                .title(concert.getTitle())
+                .description(concert.getDescription())
+                .status(concert.getStatus())
+                .build();
+    }
 }
--- src/main/java/kr/hhplus/be/server/interfaces/dto/SeatDto.java
+++ src/main/java/kr/hhplus/be/server/interfaces/dto/SeatDto.java
@@ -1,14 +1,29 @@
 package kr.hhplus.be.server.interfaces.dto;
 
+import kr.hhplus.be.server.domain.entity.Seat;
 import kr.hhplus.be.server.support.type.SeatStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class SeatDto {
     private Long seatId;
     private Long seatNumber;
     private SeatStatus status;
     private Long seatPrice;
+
+
+    public static SeatDto of(Seat seat) {
+        return SeatDto.builder()
+                .seatId(seat.getId())
+                .seatNumber(seat.getSeatNumber())
+                .status(seat.getSeatStatus())
+                .seatPrice(seat.getSeatPrice())
+                .build();
+    }
 }
