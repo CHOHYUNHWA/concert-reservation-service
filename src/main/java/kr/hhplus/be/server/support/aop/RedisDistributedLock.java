15fbe21e00fa1692018f5a05b280f742be7e96ce
CHOHYUNHWA <gusghk115@gmail.com>
2025-01-23 14:55:31 +0900

    init: Redis 분산락 구현을 위한 Configuration 설정

---
+++ src/main/java/kr/hhplus/be/server/support/aop/RedisDistributedLock.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.support.aop;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RedisDistributedLock {
+
+    String key(); // Lock의 이름 (고유값)
+    long waitTime() default 5L; // Lock획득을 시도하는 최대 시간 (ms)
+    long leaseTime() default 2L; // 락을 획득한 후, 점유하는 최대 시간 (ms)
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+}
